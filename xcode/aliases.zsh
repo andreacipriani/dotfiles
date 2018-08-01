7df338c837081b787f619bf923eb6341ce6871a9
Andrea Cipriani <andrea.cipriani@soundcloud.com>
2018-08-01 22:20:58 +0200

    Update Xcode aliases

--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -1,1 +1,5 @@
+alias xcode="open -a Xcode"
 alias watchos="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator\ \(Watch\).app"
+alias xcodetemplates='cd ~/Library/Developer/Xcode/Templates/'
+alias cleanderivedata='rm -rf ~/Library/Developer/Xcode/DerivedData'
+alias pi='pod install --repo-update'
