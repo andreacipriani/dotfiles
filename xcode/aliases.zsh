83ff3da50c17f51db65337242fa22eecee2a3338
Andrea Cipriani <andrea.cipriani@soundcloud.com>
2018-08-01 22:21:32 +0200

    Update aliases.zsh

--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -3,3 +3,22 @@ alias watchos="open /Applications/Xcode.app/Contents/Developer/Applications/Simu
 alias xcodetemplates='cd ~/Library/Developer/Xcode/Templates/'
 alias cleanderivedata='rm -rf ~/Library/Developer/Xcode/DerivedData'
 alias pi='pod install --repo-update'
+
+# Open in Xcode, credits to @orta
+openx(){ 
+  if test -n "$(find . -maxdepth 1 -name '*.xcworkspace' -print -quit)"
+  then
+    echo "Opening workspace"
+    open *.xcworkspace
+    return
+  else
+    if test -n "$(find . -maxdepth 1 -name '*.xcodeproj' -print -quit)"
+    then
+      echo "Opening project"
+      open *.xcodeproj
+      return  
+    else
+      echo "Nothing found"
+    fi
+  fi
+}
