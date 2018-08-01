1dcba0034e50002fba400b98e85c67332d5ba5c8
Andrea Cipriani <andrea.cipriani@soundcloud.com>
2018-08-01 22:24:31 +0200

    Update aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,9 @@
 alias reload!='. ~/.zshrc'
-
 alias cls='clear' # Good 'ol Clear Screen command
+alias l='ls -la'
+alias b='cd "$OLDPWD"'
+alias r='ranger'
+alias grep='grep -in --color'
+alias be='bundle exec'
+alias h="history | grep -E -v '^ *[0-9]+ *h ' | grep "
+alias pg='ping www.google.com'
