4602cf4be7923e2ef4a5a179c69a3e3d6b00f985
Andrea Cipriani <andrea.cipriani@soundcloud.com>
2018-08-04 13:02:46 +0200

    Add zsh config so that tab completes ../

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -27,6 +27,28 @@ setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share 
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
 
+# .[TAB] will complete to ../
+zstyle ':completion:*' special-dirs true
+
+# Setup fzf
+# ---------
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+  export PATH="$PATH:/usr/local/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+
+# End setup fzf
+# ---------
+
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
 setopt complete_aliases
