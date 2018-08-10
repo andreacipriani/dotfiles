5263fdcfcc7772178995cae0c55550cf36d0191a
Andrea Cipriani <andrea.cipriani@soundcloud.com>
2018-08-10 16:01:00 +0200

    Update some git configs

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -12,6 +12,7 @@ alias gpr='git pull --rebase'
 alias gst="git status -sb"
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
+alias gpo="git push --set-upstream origin $(git branch | awk '/^\* / { print $2 }')"
 alias grc="gitc rebase --continue"
 alias grm="git fetch && git rebase origin/master"
 alias grmi="git fetch && git rebase origin/master -i"
@@ -20,6 +21,7 @@ alias gall="git add ."
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
+alias gcom='git checkout master'
 alias gcb='git checkout -b'
 alias gb='git branch'
 alias gac='git add -A && git commit -m'
@@ -27,5 +29,6 @@ alias grbtaketheirs="git checkout --ours . && git add ."
 alias grbtakemine="git checkout --theirs . && git add ."
 alias gclean= "git gc --aggressive && git prune"
 alias gcpc="gitc cherry-pick --continue"
+alias gcm="git commit -m"
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
