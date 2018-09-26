af7c5d81d09ed7ed672709eadc706daff9b9d5e1
Andrea Cipriani <andrea.cipriani@soundcloud.com>
2018-09-26 17:26:25 +0200

    Update soundcloud aliases

--- soundcloud/aliases.zsh
+++ soundcloud/aliases.zsh
@@ -1,2 +1,3 @@
 alias goios="cd ~/Code/soundcloud/ios"
 alias scloc="open ~/Code/soundcloud/ios/Frameworks/Features/Sources/Localization/Localization.bundle/en.lproj/Localizable.strings"
+alias scformatpush="sc-format && git add . && git commit --amend --no-edit && git push"
