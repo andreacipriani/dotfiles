826c4911c66991b5b624b604668e32b78b28052d
Andrea Cipriani <andrea.cipriani@soundcloud.com>
2018-10-03 16:13:41 +0200

    Add private key on git install

--- git/install.sh
+++ git/install.sh
@@ -2,3 +2,4 @@ git config --global pull.rebase true
 git config --global rebase.autoStash true 
 git config --global rerere.enabled true
 git config --global core.editor "subl -n -w"
+ssh-add -K ~/.ssh/id_rsa
